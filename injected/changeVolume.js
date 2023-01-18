9867a355fb69b8ba5cdb9ad48ef2c3e36f5791b0
Allen Cao <allen.m.cao@gmail.com>
2023-01-18 08:19:06 -0800

    Record per channel volume change and set on video load.

--- injected/changeVolume.js
+++ injected/changeVolume.js
@@ -1,37 +1,49 @@
-// Get or set volume depending on message content.
+// MAIN EVENT LISTENER
 window.addEventListener("message", (event) => {
+	// Get Volume
     if (event.data.command == "tvc_get_volume") {
         window.postMessage({ command: "tvc_get_volume_reply", data: getVideoPlayer()?.getVolume() }, "*");
     }
+	// Set volume
     else if (event.data.command == "tvc_set_volume") {
         getVideoPlayer()?.setVolume(event.data.data);
     }
+	// Handle initial step of channel load by getting channel name through video player.
+	else if (event.data.command == "tvc_channel_load") {
+		// Get channel name from video player (must be done in injected script to get video player path)
+		const channelName = getVideoPlayer()?.core?.state?.path?.split('hls/').pop().split('.')[0];
+
+		// Set the stored channel volume (must be done in content script to access chrome stored sync values)
+		window.postMessage({ command: "tvc_channel_load_set_volume", data: channelName }, "*");
+
+		// Add volume listener for this channel (must be done in content script to store chrome sync values)
+		window.postMessage({ command: "tvc_add_volume_listener", data: channelName }, "*");
+    }
 });
 
-
 // https://github.com/cleanlock/VideoAdBlockForTwitch/blob/6464358b3991f8111ee594e15189bd56d0851e0e/firefox/content.js#L755
-function findReactNode(root, constraint) {
-	if (root.stateNode && constraint(root.stateNode)) {
-		return root.stateNode;
-	}
-	let node = root.child;
-	while (node) {
-		const result = findReactNode(node, constraint);
-		if (result) {
-			return result;
-		}
-		node = node.sibling;
-	}
-	return null;
-}
-
 function getVideoPlayer(){
-	var reactRootNode = null;
-	var rootNode = document.querySelector('#root');
+	function findReactNode(root, constraint) {
+		if (root.stateNode && constraint(root.stateNode)) {
+			return root.stateNode;
+		}
+		let node = root.child;
+		while (node) {
+			const result = findReactNode(node, constraint);
+			if (result) {
+				return result;
+			}
+			node = node.sibling;
+		}
+		return null;
+	}
+
+	let reactRootNode = null;
+	const rootNode = document.querySelector('#root');
 	if (rootNode && rootNode._reactRootContainer && rootNode._reactRootContainer._internalRoot && rootNode._reactRootContainer._internalRoot.current) {
 		reactRootNode = rootNode._reactRootContainer._internalRoot.current;
 	}
-	videoPlayer = findReactNode(reactRootNode, node => node.setPlayerActive && node.props && node.props.mediaPlayerInstance);
+	const videoPlayer = findReactNode(reactRootNode, node => node.setPlayerActive && node.props && node.props.mediaPlayerInstance);
 	videoPlayer = videoPlayer && videoPlayer.props && videoPlayer.props.mediaPlayerInstance ? videoPlayer.props.mediaPlayerInstance : null;
 	return videoPlayer;
 }
--- scripts/content-script.js
+++ scripts/content-script.js
@@ -26,19 +26,41 @@
   
 //   window.twitchVolumeGainNode.gain.value = volume
 // }
+window.addEventListener("message", (message) => {
+    if (message.data.command == "tvc_add_volume_listener") {
+        const channelName = message.data.data;
+        const volumeControls = document.querySelector('.player-controls__left-control-group');
+        // const volumeControls = document.querySelector("input[id^='player-volume-']");
+        volumeControls?.addEventListener("change", (event) => {
+            // event.stopPropagation();
 
-const vol = 1;
+            const channelName = message.data.data;
+            const vol = event?.target?.value
+            chrome.storage.sync.get({'channelVolumes': {}}).then((result) => {
+                result.channelVolumes[channelName] = vol;
+                chrome.storage.sync.set(result);
+            });
+        });
+    } else if (message.data.command == "tvc_channel_load_set_volume") {
+        chrome.storage.sync.get({'channelVolumes': {}}).then((result) => {
+            const channelName = message.data.data
+            const vol = parseFloat(result.channelVolumes[channelName]);
+			window.postMessage({ command: "tvc_set_volume", data:vol}, "*");
+		});
+    }
+});
 
-var changeVolume = document.createElement('script');
+const video = document.querySelector('video')
+video.addEventListener('loadedmetadata', () => {
+    window.postMessage({ command: "tvc_channel_load"}, "*");
+})
+
+
+
+let changeVolume = document.createElement('script');
 changeVolume.src = chrome.runtime.getURL('injected/changeVolume.js');
 changeVolume.onload = function() {
     this.remove();
-    
-    window.postMessage({ command: "tvc_set_volume", data: vol }, "*"); 
-    console.log("sent set volume msg");
-
-    window.postMessage({ command: "tvc_get_volume"}, "*"); 
-    console.log("sent get volume msg");
 }; 
 (document.head || document.documentElement).appendChild(changeVolume);
 
